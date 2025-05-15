e375f0eeadb65d09c1688342d6d724a4dce0c765
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-05-16 00:54:31 +0600

    Beautiful Matix.cpp

---
+++ Beautiful Matix.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int m[5][5];
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            cin >> m[i][j];
+        }
+    }
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            if(m[i][j] == 1){
+                cout << abs(2-i) + abs(2-j) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
