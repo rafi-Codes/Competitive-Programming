349f73092b46704d53f37605f82e9d77637c9e29
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-06-18 17:27:13 +0600

    Create Two Interval.cpp

---
+++ Two Interval.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    long long L1, R1, L2, R2;
+    cin >> L1 >> R1 >> L2 >> R2;
+    
+    if (L1 == R1 && L2 == R2 && L1 == L2) { 
+        cout << L1 << " " << R1 << endl;
+    }
+    else if (L1 > R2 || L2 > R1) { 
+        cout << -1;
+    }
+    else { // Overlapping range
+        cout << max(L1, L2) << " " << min(R1, R2) << endl;
+    }
+
+    return 0;
+}
