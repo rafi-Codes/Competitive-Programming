c6df9c7d03242e49914ca0106e2f719d7a63835a
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-06-20 13:10:24 +0600

    Create 17A - Noldbach problem.cpp

---
+++ 17A - Noldbach problem.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <bool> generatePrimes(int n) {
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i * i <= n; ++i) {
+        if (prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    return prime;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<bool> is_prime = generatePrimes(n);
+
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    int Count = 0;
+    for (int i = 0; i + 1 < primes.size(); ++i) {
+        int sum = primes[i] + primes[i + 1] + 1;
+        if (sum <= n && is_prime[sum]) {
+            Count++;
+        }
+    }
+
+    if (Count >= k) {
+        cout << "YES" << endl;
+    } 
+    
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
