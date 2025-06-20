3de34bbee9b0be1930d1ada41e720e3411b15afa
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-06-20 17:52:16 +0600

    Update 17A - Noldbach problem.cpp

--- 17A - Noldbach problem.cpp
+++ 17A - Noldbach problem.cpp
@@ -5,7 +5,7 @@ vector <bool> generatePrimes(int n) {
     vector<bool> prime(n + 1, true);
     prime[0] = prime[1] = false;
 
-    for (int i = 2; i * i <= n; ++i) {
+    for (int i = 2; i * i <= n; i++) {
         if (prime[i]) {
             for (int j = i * i; j <= n; j += i) {
                 prime[j] = false;
@@ -23,14 +23,14 @@ int main() {
     vector<bool> is_prime = generatePrimes(n);
 
     vector<int> primes;
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 2; i <= n; i++) {
         if (is_prime[i]) {
             primes.push_back(i);
         }
     }
 
     int Count = 0;
-    for (int i = 0; i + 1 < primes.size(); ++i) {
+    for (int i = 0; i + 1 < primes.size(); i++) {
         int sum = primes[i] + primes[i + 1] + 1;
         if (sum <= n && is_prime[sum]) {
             Count++;
